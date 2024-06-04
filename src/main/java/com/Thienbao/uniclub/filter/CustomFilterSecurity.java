46708f5a54025bc4ff4f102604d0acdce9e0ce0b
Thienbao6868 <thienbao3018@gmail.com>
2024-06-04 13:24:13 +0700

    Tạo chứng thực khi mã hoá token trả về true

--- src/main/java/com/Thienbao/uniclub/filter/CustomFilterSecurity.java
+++ src/main/java/com/Thienbao/uniclub/filter/CustomFilterSecurity.java
@@ -6,10 +6,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class CustomFilterSecurity extends OncePerRequestFilter {
@@ -25,8 +29,14 @@ public class CustomFilterSecurity extends OncePerRequestFilter {
         if( authorValue != null && authorValue.startsWith("Bearer ") ){
             String token = authorValue.substring(7);
             if(!token.equals("")){
-                boolean check = jwtHelper.decodeToken(token);
-                System.out.print(check);
+                boolean isSuccess = jwtHelper.decodeToken(token);
+                if(isSuccess){
+
+                    UsernamePasswordAuthenticationToken tokenAuthen = new UsernamePasswordAuthenticationToken("","",new ArrayList<>());
+
+                    SecurityContext context = SecurityContextHolder.getContext();
+                    context.setAuthentication(tokenAuthen);
+                }
             }
         }
 
